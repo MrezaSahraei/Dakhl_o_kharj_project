cffed41440f9f3952ff5d18bba5eaada72bdfd75
mreza <you@example.com>
2025-11-23 16:19:02 +0330

    feat: Define explicit API paths for Category and Transaction models

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
-
-name = 'api'
+from views import (CategoryListCreateAPIView, CategoryRetrieveUpdateDestroyAPIView,
+                   TransactionListCreateAPIView, TransactionRetrieveUpdateDestroyAPIView)
+app_name = 'api'
 
 urlpatterns = [
+    path('categories/',CategoryListCreateAPIView.as_view(),name='category-list-create'),
 
+    path('categories/<int:pk>/',CategoryRetrieveUpdateDestroyAPIView.as_view(),name='category-detail'),
+
+    path('transactions/',TransactionListCreateAPIView.as_view(),name='transaction-list-create'),
+
+    path('transactions/<int:pk>/',TransactionRetrieveUpdateDestroyAPIView.as_view(),name='transaction-detail'),
 ]
