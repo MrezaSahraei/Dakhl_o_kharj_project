16723df608db3a278abde02caed773e0abf58c5c
mreza <suhrayi@gmail.com>
2025-11-24 02:01:04 +0330

    feat: Define explicit API paths for Budgeting models

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from views import (CategoryListCreateAPIView, CategoryRetrieveUpdateDestroyAPIView,
-                   TransactionListCreateAPIView, TransactionRetrieveUpdateDestroyAPIView)
+from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('categories/',CategoryListCreateAPIView.as_view(),name='category-list-create'),
+    path('categories/', views.CategoryListCreateAPIView.as_view(),name='category-list-create'),
 
-    path('categories/<int:pk>/',CategoryRetrieveUpdateDestroyAPIView.as_view(),name='category-detail'),
+    path('categories/<int:pk>/', views.CategoryRetrieveUpdateDestroyAPIView.as_view(),name='category-detail'),
 
-    path('transactions/',TransactionListCreateAPIView.as_view(),name='transaction-list-create'),
+    path('transactions/', views.TransactionListCreateAPIView.as_view(),name='transaction-list-create'),
 
-    path('transactions/<int:pk>/',TransactionRetrieveUpdateDestroyAPIView.as_view(),name='transaction-detail'),
+    path('transactions/<int:pk>/', views.TransactionRetrieveUpdateDestroyAPIView.as_view(),name='transaction-detail'),
+
+    path('budgeting/', views.BudgetingListCreateAPIView.as_view(), name='budget-list-create'),
+
+    path('budgeting/<int:pk>', views.BudgetingRetrieveUpdateDestroyAPIView.as_view(), name='budget-detail'),
 ]
